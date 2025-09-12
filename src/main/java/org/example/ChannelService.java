78bf6ed6e2749cbea835e00d9c4dc0ebac858341
mohgh-04 <mohammmedgh0@gmail.com>
2025-09-13 00:31:42 +0300

    تم الحل

--- src/main/java/org/example/ChannelService.java
+++ src/main/java/org/example/ChannelService.java
@@ -1,19 +1,73 @@
 package org.example;
 
+import org.example.observer.Observer;
 import org.example.subject.Channel;
-import org.example.subject.Subject;
+
+import java.util.*;
 
 public class ChannelService {
+    private final Map<String, Channel> channels = new HashMap<>();
 
-    public void createNewChannel(Channel channel) {
-        // TODO: implement
+    public boolean createChannel(String name) {
+        if (name == null || name.isBlank() || this.channels.containsKey(name)) {
+            return false;
+        }
+        this.channels.put(name, new Channel(name));
+        return true;
     }
 
-    public void subscribeToChannel(Channel channel, Subject subject) {
-        // TODO: implement
+    public boolean deleteChannel(String name) {
+        if (name == null || !channels.containsKey(name)) {
+            return false;
+        }
+        this.channels.remove(name);
+        return true;
     }
 
-    public void newContentNotification(Channel channel, String message) {
-        // TODO: implement
+    public Optional<Channel> getChannel(String name) {
+        if (name == null) {
+            return Optional.empty();
+        }
+        return Optional.ofNullable(channels.get(name));
+    }
+
+    public boolean subscribe(String channelName, Observer user) {
+        if (channelName == null || user == null) {
+            return false;
+        }
+        Channel channel = channels.get(channelName);
+        if (channel == null) {
+            return false;
+        }
+        channel.subscribe(user);
+        return true;
+    }
+
+    public boolean unsubscribe(String channelName, Observer user) {
+        if (channelName == null || user == null) {
+            return false;
+        }
+        Channel channel = this.channels.get(channelName);
+        if (channel == null) {
+            return false;
+        }
+        channel.unsubscribe(user);
+        return true;
+    }
+
+    public boolean upload(String channelName, String videoTitle) {
+        if (channelName == null || videoTitle == null) {
+            return false;
+        }
+        Channel channel = this.channels.get(channelName);
+        if (channel == null) {
+            return false;
+        }
+        channel.uploadVideo(videoTitle);
+        return true;
+    }
+
+    public Set<String> listChannels() {
+        return new HashSet<>(this.channels.keySet());
     }
 }
